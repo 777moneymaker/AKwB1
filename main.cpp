0a61997861a1c52475bde6598a107912a285432b
777moneymaker <adeeeq77@gmail.com>
2019-10-16 21:27:37 +0200

    Some new functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+
 
 using namespace std;
 
@@ -12,45 +14,84 @@ public:
 
 class Graph{
 public:
-    int num_of_vert;
+    int num_of_vert = 0;
     Vertex **adj_matrix = new Vertex*[50];
 
-    Graph(int num_of_vert=0){
+    Graph(int num_of_vert=5){
         this->num_of_vert = num_of_vert;
         for(int i=0;i<50;i++){
-            adj_matrix[i] = new Vertex[50];
+            this->adj_matrix[i] = new Vertex[50];
+        }
+        for(int i=0;i<50;i++){
+            for(int j=0;j<50;j++) {
+                this->adj_matrix[i][j].key = rand() % 2;
+            }
         }
     }
 
-    void saveGraph(){
-        // TODO saves graph to txt file
+    void saveGraph(int iters){
+        fstream file;
+        file.open("graph.txt", ios::out);
+        if(file.good()){
+            file.clear();
+            file<<this->num_of_vert<<endl;
+            for(int i=0;i<iters;i++){
+                for(int j=0;j<iters;j++){
+                    file<<this->adj_matrix[i][j].key<<" ";
+                }
+                file<<endl;
+            }
+            file.close();
+        }else{
+            cout<<"Error while opening the file"<<endl;
+        }
+
+
     }
 
-    void loadGraph(){
-        // TODO loads graph from txt
+    void loadGraph(int iters){
+        fstream file;
+        file.open("graph.txt", ios::in);
+        if(file.good()){
+            file >> this->num_of_vert;
+            for(int i=0;i<iters;i++){
+                for(int j=0;j<iters;j++){
+                    file>>this->adj_matrix[i][j].key;
+                }
+            }
+        }else{
+            cout<<"Error while opening the file"<<endl;
+        }
     }
 
-    void clearGraph()_{
-        // TODO sets all keys to 0
+    void printGraph(int iters){
+        cout<<"Number of vertices: "<<this->num_of_vert<<endl;
+        for(int i=0;i<iters;i++){
+            for(int j=0;j<iters;j++){
+                cout<<this->adj_matrix[i][j].key<<" ";
+            }
+            cout<<endl;
+        }
     }
+
+    void clearGraph(){
+        for(int i=0;i<50;i++){
+            for(int j=0;j<50;j++) {
+                this->adj_matrix[i][j].key = 0;
+            }
+        }
+    }
+
 
 };
 
 
 int main() {
-    auto *G = new Graph;
+    auto *G = new Graph(10); string filename;
 
-    for(int i=0;i<50;i++){
-        for(int j=0;j<50;j++) {
-            G->adj_matrix[i][j].key = 0;
-        }
-    }
+    G->printGraph(10);
+    G->loadGraph(10);
+    G->printGraph(10);
 
-    for(int i=0;i<50;i++){
-        for(int j=0;j<50;j++){
-            cout<<G->adj_matrix[i][j].key;
-        }
-        cout<<endl;
-    }
     return 0;
 }
